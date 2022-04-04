dcc9edd45366592ca89450b36dc1a03bba53bf50
Shaun47 <hossainshaun27@gmail.com>
2022-04-04 21:08:36 +0600

    context processor done for category

--- My Cart/mykart/category/models.py
+++ My Cart/mykart/category/models.py
@@ -2,6 +2,7 @@ from distutils.command.upload import upload
 from tabnanny import verbose
 from unicodedata import category
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -19,6 +20,9 @@ class Category(models.Model):
     class Meta:
         verbose_name = "category"
         verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('products_by_category',args=[self.slug])
 
     def __str__(self):
         return self.category_name
